fcc073f50ea94aa30bd29f4f9c1a39bbe6c0c1e2
Matthew Adams <mryan1312@outlook.com>
2024-02-29 09:28:50 -0600

    Update mount_drives.sh

--- mount_drives.sh
+++ mount_drives.sh
@@ -1,102 +1,109 @@
 #!/bin/bash
 plistfile="/Library/LaunchAgents/map_drives.plist"
 sudo touch $plistfile
+sudo chown root $plistfile
+
 #writing the plist
-echo "<?xml version="1.0" encoding="UTF-8"?>" | sudo tee -a $plistfile
-echo "<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">" | sudo tee -a $plistfile
-echo "<plist version="1.0">" | sudo tee -a $plistfile
-echo "<dict>" | sudo tee -a $plistfile
-echo "    <key>Label</key>" | sudo tee -a $plistfile
-echo "    <string>com.user.loginscript</string>" | sudo tee -a $plistfile
-echo "    <key>ProgramArguments</key>" | sudo tee -a $plistfile
-echo "    <array>" | sudo tee -a $plistfile
-echo "        <string>/bin/sh</string>" | sudo tee -a $plistfile
-echo "        <string>/Users/Shared/map_drives.sh</string>" | sudo tee -a $plistfile
-echo "    </array>" | sudo tee -a $plistfile
-echo "    <key>RunAtLoad</key>" | sudo tee -a $plistfile
-echo "    <true/>" | sudo tee -a $plistfile
-echo "    <key>KeepAlive</key>" | sudo tee -a $plistfile
-echo "    <dict>" | sudo tee -a $plistfile
-echo "        <key>SuccessfulExit</key>" | sudo tee -a $plistfile
-echo "        <false/>" | sudo tee -a $plistfile
-echo "    </dict>" | sudo tee -a $plistfile
-echo "</dict>" | sudo tee -a $plistfile
-echo "</plist>" | sudo tee -a $plistfile
+cat > $plistfile << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>com.user.loginscript</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/bin/sh</string>
+        <string>/Users/Shared/map_drives.sh</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+    <key>KeepAlive</key>
+    <dict>
+        <key>SuccessfulExit</key>
+        <false/>
+    </dict>
+</dict>
+</plist>
+EOF
+
 #Writing the script to shared folder
 scriptfile=/Users/Shared/map_drives.sh
 sudo touch $scriptfile
 sudo chmod +x $scriptfile
 sudo chmod 777 $scriptfile
-echo '#!/bin/bash' | sudo tee -a $scriptfile
-echo "# Get the current user's username and set desktop directory" | sudo tee -a $scriptfile
-echo 'user=$(whoami)' | sudo tee -a $scriptfile
-echo 'directory="$HOME/Desktop/"' | sudo tee -a $scriptfile
-echo "# Check AD group membership" | sudo tee -a $scriptfile
-echo 'groupMembership="$(id -Gn $user)"' | sudo tee -a $scriptfile
-echo "# Check for specific group memberships and mount shares accordingly" | sudo tee -a $scriptfile
-echo 'rm $directory"Build Scans.command"' | sudo tee -a $scriptfile
-echo 'rm $directory"Sales Scans.command"' | sudo tee -a $scriptfile
-echo 'rm $directory"Purchasing Scans.command"' | sudo tee -a $scriptfile
-echo 'rm $directory"Marketing Scans.command"' | sudo tee -a $scriptfile
-echo 'rm $directory"Leadership Scans.command"' | sudo tee -a $scriptfile
-echo 'rm $directory"IT Scans.command"' | sudo tee -a $scriptfile
-echo 'rm $directory"HR Scans.command"' | sudo tee -a $scriptfile
-echo 'rm $directory"Architecture Scans.command"' | sudo tee -a $scriptfile
-echo 'rm $directory"Accounting Scans.command"' | sudo tee -a $scriptfile
-echo 'if [[ $groupMembership == *"SMH_MO-JAS_Build_Access"* ]]; then' | sudo tee -a $scriptfile
-echo '    touch $directory"Build Scans.command"' | tee -a $scriptfile
-echo '    chmod +x $directory"Build Scans.command"' | tee -a $scriptfile
-echo '    echo '#!/bin/bash' | tee -a "$directory"Build Scans.command"' | tee -a $scriptfile
-echo '    echo "open \"smb://192.168.0.6/MO-JAS-Other Scans/\"" | -a $directory"Build Scans.command"' | sudo tee -a $scriptfile
-echo "fi" | sudo tee -a $scriptfile
-echo 'if [[ $groupMembership == *"SMH_MO-JAS_Sales_Access"* ]]; then' | sudo tee -a $scriptfile
-echo '    touch $directory"Sales Scans.command"' | sudo tee -a $scriptfile
-echo '    chmod +x $directory"Sales Scans.command"' | sudo tee -a $scriptfile
-echo '    echo '#!/bin/bash' | tee -a $directory"Sales Scans.command"' | sudo tee -a $scriptfile
-echo '    echo "open \"smb://192.168.0.6/MO-JAS-Sales Scans/\"" | tee -a $directory"Sales Scans.command"' | sudo tee -a $scriptfile
-echo "fi" | sudo tee -a $scriptfile
-echo 'if [[ $groupMembership == *"PurchasingTeam_Access"* ]]; then' | sudo tee -a $scriptfile
-echo '    touch $directory"Purchasing Scans.command"' | sudo tee -a $scriptfile
-echo '    chmod +x $directory"Purchasing Scans.command"' | sudo tee -a $scriptfile
-echo '    echo '#!/bin/bash' | tee -a $directory"Purchasing Scans.command"' | sudo tee -a $scriptfile
-echo '    echo "open \"smb://192.168.0.6/Purchasing Scans/\"" | tee -a $directory"Purchasing Scans.command"' | sudo tee -a $scriptfile
-echo "fi" | sudo tee -a $scriptfile
-echo 'if [[ $groupMembership == *"MarketingTeam_Access"* ]]; then' | sudo tee -a $scriptfile
-echo '    touch $directory"Marketing Scans.command"' | sudo tee -a $scriptfile
-echo '    chmod +x $directory"Marketing Scans.command"' | sudo tee -a $scriptfile
-echo '    echo '#!/bin/bash' | tee -a $directory"Marketing Scans.command"' | sudo tee -a $scriptfile
-echo '    echo "open \"smb://192.168.0.6/Marketing Scans/\"" | tee -a $directory"Marketing Scans.command"' | sudo tee -a $scriptfile
-echo "fi" | sudo tee -a $scriptfile
-echo 'if [[ $groupMembership == *"LeadershipTeam_Access"* ]]; then' | sudo tee -a $scriptfile
-echo '    touch $directory"Leadership Scans.command"' | sudo tee -a $scriptfile
-echo '    chmod +x $directory"Leadership Scans.command"' | sudo tee -a $scriptfile
-echo '    echo '#!/bin/bash' | tee -a $directory"Leadership Scans.command"' | sudo tee -a $scriptfile
-echo '    echo "open \"smb://192.168.0.6/Leadership Scans/\"" | tee -a $directory"Leadership Scans.command"' | sudo tee -a $scriptfile
-echo "fi" | sudo tee -a $scriptfile
-echo 'if [[ $groupMembership == *"ITTeam_Access"* ]]; then' | sudo tee -a $scriptfile
-echo '    touch $directory"IT Scans.command"' | sudo tee -a $scriptfile
-echo '    chmod +x $directory"IT Scans.command"' | sudo tee -a $scriptfile
-echo '    echo '#!/bin/bash' | tee -a $directory"IT Scans.command"' | sudo tee -a $scriptfile
-echo '    echo "open \"smb://192.168.0.6/IT Scans/\"" | tee -a $directory"IT Scans.command"' | sudo tee -a $scriptfile
-echo "fi" | sudo tee -a $scriptfile
-echo 'if [[ $groupMembership == *"HRTeam_Access"* ]]; then' | sudo tee -a $scriptfile
-echo '    touch $directory"HR Scans.command"' | sudo tee -a $scriptfile
-echo '    chmod +x $directory"HR Scans.command"' | sudo tee -a $scriptfile
-echo '    echo '#!/bin/bash' | tee -a $directory"HR Scans.command"' | sudo tee -a $scriptfile
-echo '    echo "open \"smb://192.168.0.6/HR Scans/\"" | tee -a $directory"HR Scans.command"' | sudo tee -a $scriptfile
-echo "fi" | sudo tee -a $scriptfile
-echo 'if [[ $groupMembership == *"ArchitectureTeam_Access"* ]]; then' | sudo tee -a $scriptfile
-echo '    touch $directory"Architecture Scans.command"' | sudo tee -a $scriptfile
-echo '    chmod +x $directory"Architecture Scans.command"' | sudo tee -a $scriptfile
-echo '    echo '#!/bin/bash' | tee -a $directory"Architecture Scans.command"' | sudo tee -a $scriptfile
-echo '    echo "open \"smb://192.168.0.6/Architecture Scans/\"" | tee -a $directory"Architecture Scans.command"' | sudo tee -a $scriptfile
-echo "fi" | sudo tee -a $scriptfile
-echo 'if [[ $groupMembership == *"AccountingTeam_Access"* ]]; then' | sudo tee -a $scriptfile
-echo '    touch $directory"Accounting Scans.command"' | sudo tee -a $scriptfile
-echo '    chmod +x $directory"Accounting Scans.command"' | sudo tee -a $scriptfile
-echo '    echo '#!/bin/bash' | tee -a $directory"Accounting Scans.command"' | sudo tee -a $scriptfile
-echo '    echo "open \"smb://192.168.0.6/Accounting Scans/\"" | tee -a $directory"Accounting Scans.command"' | sudo tee -a $scriptfile
-echo "fi" | sudo tee -a $scriptfile
+sudo cat > $scriptfile << EOF
+#!/bin/bash
+# Get the current user's username and set desktop directory
+user=$(whoami)
+directory="$HOME/Desktop/"
+# Check AD group membership
+groupMembership="$(id -Gn $user)"
+# Check for specific group memberships and mount shares accordingly
+rm $directory"Build Scans.command"
+rm $directory"Sales Scans.command"
+rm $directory"Purchasing Scans.command"
+rm $directory"Marketing Scans.command"
+rm $directory"Leadership Scans.command"
+rm $directory"IT Scans.command"
+rm $directory"HR Scans.command"
+rm $directory"Architecture Scans.command"
+rm $directory"Accounting Scans.command"
+if [[ $groupMembership == *"SMH_MO-JAS_Build_Access"* ]]; then
+    touch $directory"Build Scans.command"
+    chmod +x $directory"Build Scans.command"
+    echo '#!/bin/bash' | tee -a "$directory"Build Scans.command"
+    echo "open \"smb://192.168.0.6/MO-JAS-Other Scans/\"" | -a $directory"Build Scans.command"
+fi
+if [[ $groupMembership == *"SMH_MO-JAS_Sales_Access"* ]]; then
+    touch $directory"Sales Scans.command"
+    chmod +x $directory"Sales Scans.command"
+    echo '#!/bin/bash' | tee -a $directory"Sales Scans.command"
+    echo "open \"smb://192.168.0.6/MO-JAS-Sales Scans/\"" | tee -a $directory"Sales Scans.command"
+fi
+if [[ $groupMembership == *"PurchasingTeam_Access"* ]]; then
+    touch $directory"Purchasing Scans.command"
+    chmod +x $directory"Purchasing Scans.command"
+    echo '#!/bin/bash' | tee -a $directory"Purchasing Scans.command"
+    echo "open \"smb://192.168.0.6/Purchasing Scans/\"" | tee -a $directory"Purchasing Scans.command"
+fi
+if [[ $groupMembership == *"MarketingTeam_Access"* ]]; then
+    touch $directory"Marketing Scans.command"
+    chmod +x $directory"Marketing Scans.command"
+    echo '#!/bin/bash' | tee -a $directory"Marketing Scans.command"
+    echo "open \"smb://192.168.0.6/Marketing Scans/\"" | tee -a $directory"Marketing Scans.command"
+fi
+if [[ $groupMembership == *"LeadershipTeam_Access"* ]]; then
+    touch $directory"Leadership Scans.command"
+    chmod +x $directory"Leadership Scans.command"
+    echo '#!/bin/bash' | tee -a $directory"Leadership Scans.command"
+    echo "open \"smb://192.168.0.6/Leadership Scans/\"" | tee -a $directory"Leadership Scans.command"
+fi
+if [[ $groupMembership == *"ITTeam_Access"* ]]; then
+    touch $directory"IT Scans.command"
+    chmod +x $directory"IT Scans.command"
+    echo '#!/bin/bash' | tee -a $directory"IT Scans.command"
+    echo "open \"smb://192.168.0.6/IT Scans/\"" | tee -a $directory"IT Scans.command"
+fi
+if [[ $groupMembership == *"HRTeam_Access"* ]]; then
+    touch $directory"HR Scans.command"
+    chmod +x $directory"HR Scans.command"
+    echo '#!/bin/bash' | tee -a $directory"HR Scans.command"
+    echo "open \"smb://192.168.0.6/HR Scans/\"" | tee -a $directory"HR Scans.command"
+fi
+if [[ $groupMembership == *"ArchitectureTeam_Access"* ]]; then
+    touch $directory"Architecture Scans.command"
+    chmod +x $directory"Architecture Scans.command"
+    echo '#!/bin/bash' | tee -a $directory"Architecture Scans.command"
+    echo "open \"smb://192.168.0.6/Architecture Scans/\"" | tee -a $directory"Architecture Scans.command"
+fi
+if [[ $groupMembership == *"AccountingTeam_Access"* ]]; then
+    touch $directory"Accounting Scans.command"
+    chmod +x $directory"Accounting Scans.command"
+    echo '#!/bin/bash' | tee -a $directory"Accounting Scans.command"
+    echo "open \"smb://192.168.0.6/Accounting Scans/\"" | tee -a $directory"Accounting Scans.command"
+fi
+EOF
 
-sudo chown root $plistfile
+# Load the plist using launchctl
 sudo launchctl load $plistfile
